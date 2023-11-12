be8b45e2bf206cfe98cf63c8fa514c5b508c72cd
brusdani <daniel.brus@seznam.cz>
2023-11-12 20:35:22 +0100

    Adjusted ending text to better fix TextArea

--- src/main/java/cz/vse/danterragui/logika/Ending.java
+++ src/main/java/cz/vse/danterragui/logika/Ending.java
@@ -20,17 +20,22 @@ public class Ending {
         this.inventory = inventory;
     }
 
-    private String badEnd = "You can hear a deep voice. Babel can not be without its keeper. When one is defeated a new keeper has to take place\n"
+    private String badEnd = "You can hear a deep voice. Babel can not be without its keeper.\n"
+            +"When one is defeated a new keeper has to take place.\n"
             + "Darkness is slowly overpowering you. The last thing you remember is hearing Aiba scream.\n";
-    private String goodEnd = "You can hear a deep voice. Babel can not be without its keeper. When one is defeated a new keeper has to take its place\n"
-            +"You can hear Aiba screaming. The moonstone and sunstone united in Aiba's hands. You've never seen anything this bright\n"
-            +"The light has returned, you think to yourself. But why is it shaped exactly like Aiba. You checked again... Aiba is glowing\n"
-            +"With the newly acquired power, Aiba has banished the darkness and sunrise has returned to the lands of Danterra once again.\n";
+    private String goodEnd = "You can hear a deep voice. Babel can not be without its keeper.\n"
+            +"When one is defeated a new keeper has to take its place\n"
+            +"You can hear Aiba screaming. The moonstone and sunstone united in Aiba's hands.\n"
+            +"You've never seen anything this bright. The light has returned, you think to yourself.\n"
+            +"But why is it shaped exactly like Aiba. You checked again... Aiba is glowing\n"
+            +"With the newly acquired power, Aiba has banished the darkness \n"
+            +"and sunrise has returned to the lands of Danterra once again.\n";
 
     public String ending() {
         return "A fearsome battle has begun. You and the Keeper were exchanging blows, one after another \n"
                 +"With Aiba's and Kira's help you managed to defeat the Keeper\n"
-                +"Is this really it?, you're asking yourself. Have we won? Suddenly an unknown force started dragging you towards the throne\n";
+                +"Is this really it?, you're asking yourself. Have we won?\n"
+                +"Suddenly an unknown force started dragging you towards the throne\n";
     }
 
     /**
